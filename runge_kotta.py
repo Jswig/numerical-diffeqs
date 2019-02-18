194afab2546c06f8f761db5c2be8bfb8fe7ff73e
apoirel <apoirel@ucsc.edu>
2019-02-18 03:21:03 -0800

    finished implementing the math

--- runge_kotta.py
+++ runge_kotta.py
@@ -10,16 +10,20 @@ from matplotlib import pyplot
 
 def solve_runge_kotta(f, a, b, c, h):            
     step = (a+b) / h
-    x_vals = []
-    y_vals = []
+    t = [a]
+    y = [c]
 
+    for i in range(0, h):
+        t.append(t[i] + h)
+        u1 = y[i]
+        u2 = y[i] + 0.5*h * f(t[i], y[i])
+        u3 = y[i] + 0.5*h * f(t[i]) + 0.5*h, u2)
+        u4 = y[i] + h *f(t[i]+ 0.5*h, u3) 
+        y.append(y[i] + (1/6)*h * (f(t[i], u1) + 2*f(t[i] + 0.5*h,  u2) \
+            + 2 * f(t[i] + 0.5*h, z3) + f(t[i] + h,  u4)) )
+            
+    return [t, y]
 
-    for i in range(1, h):
-        z1 = 
-        z2 =
-        z3 =
-        z4 =
-        y_vals[i+1]
-        
-
-def plot_approx()
+def plot_approx():
+    pass
+    # FIXME
