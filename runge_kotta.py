a7dc9722a7a6c118e6adbc37dc751e83cd41c7ff
apoirel <apoirel@ucsc.edu>
2019-02-18 02:57:38 -0800

    program outline

---
+++ runge_kotta.py
@@ -0,0 +1,25 @@
+# runge_kotta.py
+#
+# Anders Poirel
+# apoirel@ucsc.edu
+# 17-02-2019
+
+
+import math
+from matplotlib import pyplot
+
+def solve_runge_kotta(f, a, b, c, h):            
+    step = (a+b) / h
+    x_vals = []
+    y_vals = []
+
+
+    for i in range(1, h):
+        z1 = 
+        z2 =
+        z3 =
+        z4 =
+        y_vals[i+1]
+        
+
+def plot_approx()
